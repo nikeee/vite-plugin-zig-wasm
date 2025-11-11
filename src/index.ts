af9588555908efa9406f1c9b7fe1baf360d2f91b
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-11 15:17:40 +0100

    Simplify flow

--- src/index.ts
+++ src/index.ts
@@ -51,53 +51,62 @@ export default function zigWasmPlugin(options: Options = {}): Plugin {
   return {
     name: "vite-plugin-zig-wasm",
     async transform(_code, id, options) {
-      if (id.endsWith(ID_SUFFIX)) {
-        const filePath = fsPathFromUrl(id);
+      if (!id.endsWith(ID_SUFFIX)) {
+        return;
+      }
 
-        const hash = getHash(cleanUrl(id));
-        const uniqWasmName = `${path.basename(filePath, ".zig")}.${hash}.wasm`;
-        const wasmPath = path.join(resolvedCacheDir, uniqWasmName);
+      const filePath = fsPathFromUrl(id);
 
-        const args = [
-          "build-lib",
-          "-dynamic",
-          "-target",
-          "wasm32-freestanding",
-          `-femit-bin=${wasmPath}`,
-          `-Drelease-${releaseMode}`,
-          `--cache-dir`,
-          resolvedZigCacheDir,
-        ];
-        if (strip) {
-          args.push(zigSelfHosted ? "-dead_strip" : "--strip");
-        }
-        if (extraArgs.length) {
-          args.push.apply(args, extraArgs);
-        }
-        args.push(filePath);
-        const result = spawnSync(zigBinPath, args, { stdio: "inherit" });
+      const hash = getHash(cleanUrl(id));
+      const uniqWasmName = `${path.basename(filePath, ".zig")}.${hash}.wasm`;
+      const wasmPath = path.join(resolvedCacheDir, uniqWasmName);
+
+      const args = [
+        "build-lib",
+        "-dynamic",
+        "-target",
+        "wasm32-freestanding",
+        `-femit-bin=${wasmPath}`,
+        `-Drelease-${releaseMode}`,
+        `--cache-dir`,
+        resolvedZigCacheDir,
+      ];
+
+      if (strip) {
+        args.push(zigSelfHosted ? "-dead_strip" : "--strip");
+      }
+
+      if (extraArgs.length) {
+        args.push.apply(args, extraArgs);
+      }
+
+      args.push(filePath);
+      const result = spawnSync(zigBinPath, args, { stdio: "inherit" });
+      if (result.error) {
+        throw result.error;
+      }
+
+      if (optimize) {
+        const optimizedFile = path.join(
+          resolvedCacheDir,
+          `wasm-optimized.${uniqWasmName}`
+        );
+        const args = ["-o", optimizedFile];
+        const extraArgs = Array.isArray(optimize)
+          ? optimize
+          : ["-Oz", "--strip-debug"];
+        const result = spawnSync(
+          wasmOptPath!,
+          [wasmPath, ...args, ...extraArgs],
+          { stdio: "inherit" }
+        );
         if (result.error) throw result.error;
+        await fs.rename(optimizedFile, wasmPath);
+      }
 
-        if (optimize) {
-          const optimizedFile = path.join(
-            resolvedCacheDir,
-            `wasm-optimized.${uniqWasmName}`
-          );
-          const args = ["-o", optimizedFile];
-          const extraArgs = Array.isArray(optimize)
-            ? optimize
-            : ["-Oz", "--strip-debug"];
-          const result = spawnSync(
-            wasmOptPath!,
-            [wasmPath, ...args, ...extraArgs],
-            { stdio: "inherit" }
-          );
-          if (result.error) throw result.error;
-          await fs.rename(optimizedFile, wasmPath);
-        }
-        return {
-          code: options?.ssr
-            ? `
+      return {
+        code: options?.ssr
+          ? `
 import * as fs from "node:fs/promises";
 
 export default async function init(opts) {
@@ -105,20 +114,20 @@ export default async function init(opts) {
   const result = await WebAssembly.instantiate(bytes, opts);
   return result.instance;
 }`
-            : `
+          : `
 import init from '${normalizePath(wasmPath)}?init';
 export default init;`,
-          map: { mappings: "" },
-        };
-      }
+        map: { mappings: "" },
+      };
     },
+
     configResolved: async (config) => {
       const pkgPath = lookupFile(config.root, ["package.json"]);
       resolvedCacheDir = cacheDir
         ? path.resolve(config.root, cacheDir)
         : pkgPath
-        ? path.join(path.dirname(pkgPath), "node_modules/.vite-plugin-zig-wasm")
-        : path.join(config.root, ".vite-plugin-zig-wasm");
+          ? path.join(path.dirname(pkgPath), "node_modules/.vite-plugin-zig-wasm")
+          : path.join(config.root, ".vite-plugin-zig-wasm");
 
       resolvedZigCacheDir = zigCacheDir
         ? path.join(config.root, zigCacheDir)
