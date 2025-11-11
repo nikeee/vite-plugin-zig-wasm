8dde232fb35e6cc438e37b3e254545abc61947da
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-11 15:11:43 +0100

    vitest -> node:test

--- src/__test__/helper.test.ts
+++ src/__test__/helper.test.ts
@@ -1,11 +1,12 @@
-import { it, expect } from 'vitest';
-import { ensureZigVersion } from '../helper';
+import { describe } from 'node:test';
 
-it("ensure zig version", () => {
+import { expect } from "expect";
+import { ensureZigVersion } from '../helper/index.ts';
 
-    expect(() => ensureZigVersion("0.9.0", ">=0.10.0")).toThrowError();
-    expect(() => ensureZigVersion("invalid", ">=0.10.0")).toThrowError();
+describe("ensure zig version", () => {
+    expect(() => ensureZigVersion("0.9.0", ">=0.10.0")).toThrow();
+    expect(() => ensureZigVersion("invalid", ">=0.10.0")).toThrow();
 
-    expect(() => ensureZigVersion("0.11.0-dev", ">=0.10.0")).not.toThrowError();
-    expect(() => ensureZigVersion("0.10.0-dev.4720+9b54c9de", ">=0.10.0")).not.toThrowError();
+    expect(() => ensureZigVersion("0.11.0-dev", ">=0.10.0")).not.toThrow();
+    expect(() => ensureZigVersion("0.10.0-dev.4720+9b54c9de", ">=0.10.0")).not.toThrow();
 })
