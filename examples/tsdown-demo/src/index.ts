2dfa0a4df8bdbce366f927a49959bfa3f1dd776b
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-12 20:30:08 +0100

    Add tsdown source

--- examples/tsdown-demo/src/index.ts
+++ examples/tsdown-demo/src/index.ts
@@ -1,1 +1,11 @@
+import compileModule from "./math.zig?compile";
+
+const module = await compileModule();
+
+const instance = await WebAssembly.instantiate(module, {
+  // import object
+});
+const result = instance.exports.add(1, 2);
+
 console.log("hi");
+console.log("Result:", result);
